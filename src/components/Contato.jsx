cacc0e6384ceb6b48f6c24599cd47e129bd23538
Elisvaldo Braga <elisvaldo123braga03@gmail.com>
2026-04-27 18:19:25 -0300

    Finalizei a seção Contato

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,42 +1,90 @@
 import { MdAlternateEmail } from "react-icons/md";
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import "../styles/Contato.css";
 
-export function Contato(){
+export function Contato() {
+  return (
+    <section id="contato">
+      <h1 className="titulo titulo-contato">Contato</h1>
+      <h2>
+        Vamos <span>Conversar</span>
+      </h2>
+      <p className="p-contato">
+        Tem um projeto em mente? <br />
+        Estou pronto para transformá-lo em realidade.
+      </p>
 
-    return (
-        <section id="contato">
-            <h1 className="titulo titulo-contado">Contato</h1>
-            <h2>Vamos <span>Conversar</span></h2>
-            <p>Tem um projeto em mente?</p>
-            <p>Estou pronto para transformá-lo em realidade.</p>
+      <div className="grid">
+        <div className="fale-comigo">
+          <h1>Fale comigo</h1>
+          <p>
+            Seja para um projeto freelance, uma oportunidade de trabalho, ou
+            apenas para trocar ideias sobre tecnologia - minha caixa de entrada
+            está aberta.
+          </p>
 
-            <div className="fale-comigo">
-                <h1>Fale comigo</h1>
-                <p>Seja para um projeto freelance, uma oportunidade de trabalho, ou apenas para trocar ideias sobre tecnologia - minha caixa de entrada está aberta.</p>
-                
-                <div className="contatos">
-                    <MdAlternateEmail />
-                    <div className="contatos-column">
-                        <p className="rede">E-mail</p>
-                        <p className="nome-rede">elisvaldo123braga03@gmail.com</p>
-                    </div>
-                </div>
-                <div className="contatos">
-                    <FaWhatsapp />
-                    <div className="contatos-column">
-                        <p className="rede">WhatsApp</p>
-                        <p className="nome-rede">+55 (88) 9 9629-2920</p>
-                    </div>
-                </div>
-                <div className="contatos">
-                    <FaLocationDot />
-                    <div className="contatos-column">
-                        <p className="rede">Localização</p>
-                        <p className="nome-rede">Aracati, CE - Brasil</p>
-                    </div>
-                </div>
+
+          <div className="contatos">
+            <MdAlternateEmail className="icon" />
+            <div className="contatos-column">
+              <p className="contato-tipo">E-mail</p>
+              <p className="contato-info">elisvaldo123braga03@gmail.com</p>
             </div>
-        </section>
-    )
+          </div>
+          <div className="contatos">
+            <FaWhatsapp className="icon" />
+            <div className="contatos-column">
+              <p className="contato-tipo">WhatsApp</p>
+              <p className="contato-info">+55 (88) 9 9629-2920</p>
+            </div>
+          </div>
+          <div className="contatos">
+            <FaLocationDot className="icon" />
+            <div className="contatos-column">
+              <p className="contato-tipo">Localização</p>
+              <p className="contato-info">Aracati, CE - Brasil</p>
+            </div>
+          </div>
+          <div className="redes-sociais">
+            <a title="GitHub" className="rede-social icon" href="#">
+              <FaGithub />
+            </a>
+            <a title="Linkedin" className="rede-social" href="#">
+              <FaLinkedin />
+            </a>
+            <a title="Instagram" className="rede-social" href="#">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+
+        <form action="#">
+          <div className="form-nome column">
+            <label htmlFor="nome">Nome Completo</label>
+            <input type="text" id="nome" placeholder="Seu nome" required />
+          </div>
+          <div className="form-email column">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="seu@email.com"
+              required
+            />
+          </div>
+          <div className="form-msg column">
+            <label htmlFor="msg">Mensagem</label>
+            <textarea
+              id="msg"
+              rows={10}
+              placeholder="Conte sobre seu projeto ou ideia..."
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Enviar mensagem</button>
+        </form>
+      </div>
+    </section>
+  );
 }
