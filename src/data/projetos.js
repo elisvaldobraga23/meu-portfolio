382d8906bd5eedadade5908dd73598eb743d4017
Elisvaldo Braga <elisvaldo123braga03@gmail.com>
2026-04-29 12:22:46 -0300

    atualizações

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -1,58 +1,24 @@
 import imgCadastro from "../assets/img/tela-cadastro-usuarios.png";
+import imgListaCompras from "../assets/img/lista-de-compras.png";
 
 export function dataProjetos() {
     return {
-        projeto1:{
+        projeto01:{
             Titulo:"Cadastro de Usuários",
             descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
             tags:["HTML5", "CSS3", "React"],
             img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
+            link: "",
             linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
+        },
+        projeto02:{
+            Titulo:"Lista de Compras",
+            descricao:"Aplicação web para gerenciamento de lista de compras, com adição, remoção e controle de itens, utilizando JavaScript puro e armazenamento local.",
+            tags:["HTML5", "CSS3", "JavaScript"],
+            img: imgListaCompras,
+            link: "https://elisvaldobraga23.github.io/lista_de_compras/",
+            linkGithub: "https://github.com/elisvaldobraga23/lista_de_compras"
         },
 
-        projeto2:{
-            Titulo:"Cadastro de Usuários",
-            descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
-            tags:["HTML5", "CSS3", "React"],
-            img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
-            linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
-        },
-
-        projeto3:{
-            Titulo:"Cadastro de Usuários",
-            descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
-            tags:["HTML5", "CSS3", "React"],
-            img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
-            linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
-        },
-
-        projeto4:{
-            Titulo:"Cadastro de Usuários",
-            descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
-            tags:["HTML5", "CSS3", "React", "HTML5", "CSS3", "React"],
-            img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
-            linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
-        },
-        
-        projeto5:{
-            Titulo:"Cadastro de Usuários",
-            descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
-            tags:["HTML5", "CSS3", "React"],
-            img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
-            linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
-        },
-        projeto6:{
-            Titulo:"Cadastro de Usuários",
-            descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
-            tags:["HTML5", "CSS3", "React"],
-            img: imgCadastro,
-            link: "https://github.com/elisvaldobraga23/cadastro_de_usuarios",
-            linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
-        }
     }
 }
