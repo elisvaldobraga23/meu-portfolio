70f6c818e34c997f396b9bf5c7d379a03292df9b
Elisvaldo Braga <elisvaldo123braga03@gmail.com>
2026-04-29 18:11:57 -0300

    Adiconei outros projetos ao portfolio

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -1,9 +1,25 @@
 import imgCadastro from "../assets/img/tela-cadastro-usuarios.png";
 import imgListaCompras from "../assets/img/lista-de-compras.png";
+import imgControleGasto from "../assets/img/controle-gastos.png";
+import imgCampoHarmonico from "../assets/img/estudo-campo-harmonico.png";
+import imgAgenciaViagem from "../assets/img/agencia-viagens.png";
+import imgTelaLogin from "../assets/img/tela-login.png";
+import imgGeradorSenha from "../assets/img/tela-gerador-de-senha.png";
+import imgCalculadoraImc from "../assets/img/calculadora-imc.png";
 
 export function dataProjetos() {
+
     return {
+
         projeto01:{
+            Titulo:"Tela Login Animada",
+            descricao:"Interface moderna de login e cadastro com transições animadas entre as telas, proporcionando uma experiência fluida e responsiva ao usuário.",
+            tags:["HTML5", "CSS3", "JavaScript"],
+            img: imgTelaLogin,
+            link: "https://elisvaldobraga23.github.io/tela-login-cadastro-animado/",
+            linkGithub: "https://github.com/elisvaldobraga23/tela-login-cadastro-animado"
+        },
+        projeto02:{
             Titulo:"Cadastro de Usuários",
             descricao:"Aplicação desenvolvida em React para gerenciamento de usuários de forma simples e intuitiva. O sistema permite cadastrar, listar e remover usuários dinamicamente.",
             tags:["HTML5", "CSS3", "React"],
@@ -11,7 +27,7 @@ export function dataProjetos() {
             link: "",
             linkGithub: "https://github.com/elisvaldobraga23/cadastro_de_usuarios"
         },
-        projeto02:{
+        projeto03:{
             Titulo:"Lista de Compras",
             descricao:"Aplicação web para gerenciamento de lista de compras, com adição, remoção e controle de itens, utilizando JavaScript puro e armazenamento local.",
             tags:["HTML5", "CSS3", "JavaScript"],
@@ -19,6 +35,47 @@ export function dataProjetos() {
             link: "https://elisvaldobraga23.github.io/lista_de_compras/",
             linkGithub: "https://github.com/elisvaldobraga23/lista_de_compras"
         },
+        projeto04:{
+            Titulo:"Controle de Gastos",
+            descricao:"Sistema web de controle financeiro pessoal que permite registrar despesas, acompanhar gastos por categoria e exportar dados para Excel. Desenvolvido com JavaScript puro e armazenamento local, com foco em praticidade, organização e uso sem back-end.",
+            tags:["HTML5", "CSS3", "JavaScript", "SheetJS"],
+            img: imgControleGasto,
+            link: "https://elisvaldobraga23.github.io/controle_de_gastos/",
+            linkGithub: "https://github.com/elisvaldobraga23/controle_de_gastos"
+        },
+        projeto05:{
+            Titulo:"Estudo Campo Harmônico",
+            descricao:"Aplicação web para estudo de campo harmônico, com perguntas interativas, múltipla escolha e feedback em tempo real, desenvolvida com JavaScript puro.",
+            tags:["HTML5", "CSS3", "JavaScript"],
+            img: imgCampoHarmonico,
+            link: "https://elisvaldobraga23.github.io/estudo_campo_harmonico/",
+            linkGithub: "https://github.com/elisvaldobraga23/estudo_campo_harmonico"
+        },
+        projeto06:{
+            Titulo:"Agencia de Viagem",
+            descricao:"Landing page de viagem sobre Canoa Quebrada, com seções informativas, desenvolvida como desafio do curso DevStart do SENAI-SC.",
+            tags:["HTML5", "CSS3"],
+            img: imgAgenciaViagem,
+            link: "https://elisvaldobraga23.github.io/Agencia-de-Viagem/",
+            linkGithub: "https://github.com/elisvaldobraga23/Agencia-de-Viagem"
+        },
+        projeto07:{
+            Titulo:"Gerador de Senhas",
+            descricao:"Gerador de senhas personalizável que cria combinações seguras com diferentes tipos de caracteres, permitindo ajuste de tamanho e cópia rápida com feedback visual.",
+            tags:["HTML5", "CSS3", "JavaScript"],
+            img: imgGeradorSenha,
+            link: "https://elisvaldobraga23.github.io/gerador-senha/",
+            linkGithub: "https://github.com/elisvaldobraga23/gerador-senha"
+        },
+        projeto08:{
+            Titulo:"Calculadora - IMC",
+            descricao:"Aplicação web que calcula o Índice de Massa Corporal a partir do peso e altura informados, exibindo o resultado com classificação e um indicador visual interativo.",
+            tags:["HTML5", "CSS3", "JavaScript"],
+            img: imgCalculadoraImc,
+            link: "https://elisvaldobraga23.github.io/calculadora_imc/",
+            linkGithub: "https://github.com/elisvaldobraga23/calculadora_imc"
+        },
 
     }
+
 }
